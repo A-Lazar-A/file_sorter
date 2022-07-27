8b646d550468407331e4c5f09f8eb0401ac905fd
A-Lazar-A <fub.lazar@mail.ru>
2022-07-28 02:21:30 +0300

    main logic is done

--- main.py
+++ main.py
@@ -20,6 +20,42 @@ def folder_name(extension: str) -> str:
     for folder, extensions in folder_for_extensions.items():
         if extension in extensions:
             return folder
+    return 'Nope'
 
 
+@click.command()
+@click.argument('path')
+def file_sorter(path: str) -> None:
+    """
 
+    """
+    click.echo(text2art('File Sorter'))
+    if not os.path.exists(path):
+        click.echo(f'No such directory: {path}')
+        exit(1)
+    files = os.listdir(path)
+
+    for file in files:
+        filename, extension = os.path.splitext(file)
+        extension = extension[1:]
+
+        if not extension:
+            continue
+
+        new_folder = folder_name(extension)
+
+        if new_folder == 'Nope':
+            if os.path.exists(path + '/' + extension):
+                shutil.move(path + '/' + file, path + '/' + extension)
+            else:
+                os.makedirs(path + '/' + extension)
+                shutil.move(path + '/' + file, path + '/' + extension)
+        elif os.path.exists(path + '/' + new_folder):
+            shutil.move(path + '/' + file, path + '/' + new_folder)
+        else:
+            os.makedirs(path + '/' + new_folder)
+            shutil.move(path + '/' + file, path + '/' + new_folder)
+
+
+if __name__ == '__main__':
+    file_sorter()
